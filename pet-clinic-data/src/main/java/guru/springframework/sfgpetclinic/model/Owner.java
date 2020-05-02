25e9df8a18d3cbf5c13511ac1d51feb8217e817b
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-05-02 20:41:02 +0200

    Added visits, updated owner with pet relationship. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -5,10 +5,17 @@
  */
 package guru.springframework.sfgpetclinic.model;
 
-/**
- *
- * @author None
- */
+import java.util.Set;
+
 public class Owner extends Person
 {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
