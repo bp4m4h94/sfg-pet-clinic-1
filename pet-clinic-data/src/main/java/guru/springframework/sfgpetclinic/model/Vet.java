f7252841d44d8e82c43601e6ae4594ab8fff83b3
Martin van Zyl <martin.vanzyl45@gmail.com>
2020-05-02 20:49:42 +0200

    Added specialities, added to vet. closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -5,11 +5,17 @@
  */
 package guru.springframework.sfgpetclinic.model;
 
-/**
- *
- * @author None
- */
+import java.util.Set;
+
 public class Vet extends Person
 {
-    
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
